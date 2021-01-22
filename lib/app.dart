a0bb610b8f8d9442226e530a1c2b5dcdee5c8a16
jakubjaniec <jakubjaniec@aol.com>
2021-01-22 16:09:51 +0100

    Build basic folder stucture

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:food_app_ui/ui/views/home.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/utils/theme.dart
@@ -0,0 +1,20 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class ThemeColors {
+  // #040408 - black
+  // #40364A - purple
+  // #271FE2 - dark blue
+  // #EAE9EA - creamy
+  // #C22F24 - red
+  // #8A8898 - greyish
+  // #AA9FAA - pinkish
+  // 373BA9 - dark blue
+
+  final black = HexColor('#040408');
+  final purple = HexColor('#40364A');
+  final darkBlue = HexColor('#040408');
+  final creamy = HexColor('#040408');
+  final red = HexColor('#040408');
+  final greyish = HexColor('#040408');
+  final pinkish = HexColor('#040408');
+}
